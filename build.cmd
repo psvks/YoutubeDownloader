e3b422e6a1009730cfce3aad0bf3901165ccddc3
psvks <113241132+psvks@users.noreply.github.com>
2023-12-09 23:35:46 +0100

    Bug Fixes and Made major updates

---
+++ build.cmd
@@ -0,0 +1,1 @@
+pyinstaller --noconfirm --onefile --console  "C:/Users/usr/Documents/GitHub/YoutubeDownloader/main.py"
--- main.py
+++ main.py
@@ -1,31 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for
+import argparse
 from pytube import YouTube
-import os
+from colorama import *
 
-app = Flask(__name__)
-DOWNLOAD_FOLDER = 'static'
-app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+def download_video(url, format):
+    try:
+        yt = YouTube(url)
+        if format == "mp4":
+            video_stream = yt.streams.filter(file_extension='mp4').get_highest_resolution()
+        elif format == "only_audio":
+            video_stream = yt.streams.filter(only_audio=True).first()
+        else:
+            print("Invalid format. Use: 'ytdownloader.exe https://youtube.com/watch?v=1hdAAZzgCuk mp4' or 'ytdownloader.exe https://youtube.com/watch?v=1hdAAZzgCuk only_audio'")
+            return
+        video_title = yt.title
+        download_path = f"{video_title}.{video_stream.subtype}"
+        total_size = video_stream.filesize
+        print(f'YTDownloader will be downloading: {Fore.LIGHTCYAN_EX}{total_size}{Fore.RESET} bytes')
+        video_stream.download()
+        print("Download complete.")
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
 
-# Index
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    video_url = None
-
-    if request.method == 'POST':
-        url = request.form['url']
-
-        try:
-            # Crear objeto YouTube
-            yt = YouTube(url)
-            video = yt.streams.get_highest_resolution()
-            video_path = os.path.join(app.config['DOWNLOAD_FOLDER'])
-            video.download(output_path=app.config['DOWNLOAD_FOLDER'])
-            video_url = url_for('static', filename=yt.title + '.mp4')
-        except Exception as e:
-            error_message = f"Ocurrió un error: {e}"
-            return render_template('index.html', error_message=error_message, video_url=video_url)
-
-    return render_template('index.html', video_url=video_url)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Descargar videos de YouTube.")
+    parser.add_argument("url", help="URL del video de YouTube")
+    parser.add_argument("format", choices=["mp4", "only_audio"], help="Formato de salida ('mp4' o 'mp3')")
+    args = parser.parse_args()
+    download_video(args.url, args.format)
