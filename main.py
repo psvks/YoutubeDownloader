859db12b90afd4034ff82cabbc27dcd2765dc0c9
psvks <113241132+psvks@users.noreply.github.com>
2023-12-10 22:38:33 +0100

    Added CORS

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import os
 from moviepy.editor import *
 import shutil
 import automaticDel
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 VIDEO_DIR = 'static'
 if not os.path.exists(VIDEO_DIR):
