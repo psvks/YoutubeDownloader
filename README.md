6a1a525ee733de1b4df322e7ddd211eb4b58ebf7
psvks <113241132+psvks@users.noreply.github.com>
2023-12-10 20:31:48 +0100

    web 1

--- README.md
+++ README.md
@@ -8,22 +8,6 @@ Ensure you have Python 3.12.0 installed on your system. To install the necessary
 ```bash
 pip install -r requirements.txt
 ```  
-### Usage
-To download a video, run the following command:  
-`python main.py <urltoyoutube> <format>`  
-Replace `<urltoyoutube>` with the URL of the YouTube video you want to download and `<format>` with the desired format (mp4 or mp3).  
+### Web
+This is the web version of YTDownloader, you can use this on a server or on your own APP.
 
-## Executable Tutorial
-Follow these steps to create and use the executable:  
-
-### Install Requirements:
-Ensure you have Python 3.12.0 installed, and then run the following command to install the required libraries:  
-`pip install -r requirements.txt`  
-
-### Compile the Executable:
-Open the Build file in the source code and edit it to include your file path. For example:  
-```pyinstaller --noconfirm --onefile --console "C:/Users/usr/Documents/GitHub/YoutubeDownloader/main.py"```  
-### Run the Executable:
-Open a CMD terminal, navigate to the directory containing your executable, and execute the following command:  
-`main.exe <urltoyoutube> <format>`  
-Replace `<urltoyoutube>` with the YouTube video URL and `<format>` with the desired format (mp4 or mp3).  
--- main.py
+++ main.py
@@ -1,67 +1,70 @@
-import argparse
+from flask import Flask, render_template, request, send_from_directory
 from pytube import YouTube
-from colorama import *
+import os
 from moviepy.editor import *
-from hurry.filesize import verbose, size
-from tqdm import tqdm
+
+app = Flask(__name__)
+
+VIDEO_DIR = 'static'
+if not os.path.exists(VIDEO_DIR):
+    os.makedirs(VIDEO_DIR)
+
+@app.route('/video', methods=['GET'])
+def video():
+    video_id = request.args.get('id')
+    video_path = os.path.join(VIDEO_DIR, f'{video_id}.mp4')
+    destination = os.path.join(VIDEO_DIR)
+    if not os.path.exists(video_path):
+        youtube_url = f'https://www.youtube.com/watch?v={video_id}'
+        download_video(youtube_url, destination, video_id)
+
+    video_filename = os.path.basename(video_path)
+    return render_template('video.html', video_filename=video_filename)
 
 
-def makeVidFolder():
-    folder_name = "videos"
-    folder_path = os.path.join(os.getcwd(), folder_name)
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
+@app.route('/audio', methods=['GET'])
+def audio():
+    video_id = request.args.get('id')
+    audio_path = os.path.join(VIDEO_DIR, f'{video_id}.mp3')
+    video_path = os.path.join(VIDEO_DIR, f'{video_id}.mp4')
+    destination = os.path.join(VIDEO_DIR)
+    if not os.path.exists(video_path):
+        youtube_url = f'https://www.youtube.com/watch?v={video_id}'
+        download_video(youtube_url, destination, video_id)
+        if not os.path.exists(audio_path):
+            convert_mp3(video_path=video_path, destination=destination, id=video_id)
     else:
-        pass
-    if not os.path.exists(os.path.join(os.getcwd(), 'audio')):
-        os.makedirs(os.path.join(os.getcwd(), 'audio'))
-    else:
-        pass
+        if not os.path.exists(audio_path):
+            convert_mp3(video_path=video_path, destination=destination, id=video_id)
+    
+    audio_filename = os.path.basename(audio_path)
+    return render_template('audio.html', audio_filename=audio_filename)
 
-def downloadVideo(url, format, debug):
-    makeVidFolder()
-    folder_video = os.path.join(os.getcwd(), 'videos')
-    folder_audio = os.path.join(os.getcwd(), 'audio')
-    try:
-        def progress_function(chunk, file_handle, bytes_remaining):
-            current = ((total_size - bytes_remaining) / total_size)
-            percent = int(current * 100)
-            with tqdm(total=100, desc="Downloading", unit="%", position=0, leave=True) as pbar:
-                pbar.update(percent - pbar.n)
+def download_video(url, output_path, id):
+    yt = YouTube(url)
+    video_stream = yt.streams.filter(file_extension='mp4').get_highest_resolution()
+    video_stream.download(output_path, filename=f'{id}.mp4')
 
-        yt = YouTube(url, on_progress_callback=progress_function)
-        if format == "mp4":
-            video_stream = yt.streams.filter(file_extension='mp4').get_highest_resolution()
-        elif format == "only_audio":
-            video_stream = yt.streams.filter(only_audio=True).first()
-        elif format == "mp3":
-            video_stream = yt.streams.filter(file_extension='mp4').first()
-        else:
-            print("Invalid format. Use: 'ytdownloader.exe https://youtube.com/watch?v=1hdAAZzgCuk mp4' or 'ytdownloader.exe https://youtube.com/watch?v=1hdAAZzgCuk only_audio'")
-            return
-        video_title = yt.title
-        download_path = f"{video_title}.{video_stream.subtype}"
-        total_size = video_stream.filesize
-        print(f'{Fore.LIGHTYELLOW_EX}YTDownloader{Fore.RESET} will be downloading: {Fore.LIGHTCYAN_EX}{size(total_size, system=verbose)}{Fore.RESET}')
-        print(f'{Fore.LIGHTCYAN_EX}Writing file.{Fore.RESET}')
-        if format == "mp3":
-            video_stream.download(output_path=folder_video)
-            video = VideoFileClip(f"{folder_video}\\{yt.title}.mp4")
-            print(f'{Fore.LIGHTCYAN_EX}Writing .mp3 file.{Fore.RESET}')
-            video.audio.write_audiofile(f"{folder_audio}\\{yt.title}.mp3", verbose=False, logger=None)
-            if debug:
-                print(video_stream.get_file_path())
-            print(f"{Fore.LIGHTGREEN_EX}Conversion and download complete.")
-        else:
-            video_stream.download(output_path=folder_video)
-            print(f'{Fore.LIGHTGREEN_EX}Download complete.')
-    except Exception as e:
-        print(f"Error: {e}")
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Download youtube videos")
-    parser.add_argument("url", help="URL of the youtube video")
-    parser.add_argument("format", choices=["mp4", "only_audio", "mp3"], help="Format. ('mp4', 'only_audio', 'mp3')")
-    parser.add_argument("--debug", action='store_true', help="Returns were the MP3 or MP4 is. (For servers or other things.)")
-    args = parser.parse_args()
-    downloadVideo(args.url, args.format, args.debug)
+def convert_mp3(video_path, destination, id):
+    video = VideoFileClip(video_path)
+    video.audio.write_audiofile(f"{destination}\\{id}.mp3", verbose=False, logger=None)
+
+@app.route('/videos/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(VIDEO_DIR, filename)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/license')
+def license():
+    return render_template("license.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
